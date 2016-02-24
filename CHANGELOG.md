25104f2acb06bde635318deb8f27adc66398a136
Sal Niro <salvatore.niro@outlook.com>
2016-02-23 21:08:34 -0700

    include optional details callback. docs update. adding more tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 2.0.2 (02/23/2016)
+
+- Include optional callback parameter which returns granular details from underlying clean-css process based on supplied options
+- Addition of more unit tests
+- README doc reflects new changes
+
 ## 2.0.1 (02/22/2016)
 
 - Initial release post-fork from [gulp-minify-css](https://github.com/murphydanger/gulp-minify-css)
--- index.js
+++ index.js
@@ -1,76 +1,97 @@
-'use strict';
-
-var path = require('path');
-
 var applySourceMap = require('vinyl-sourcemaps-apply');
 var CleanCSS = require('clean-css');
 var objectAssign = require('object-assign');
+var path = require('path');
 var PluginError = require('gulp-util').PluginError;
 var Transform = require('readable-stream/transform');
 var VinylBufferStream = require('vinyl-bufferstream');
 
-module.exports = function gulpCleanCSS(options) {
-  options = options || {};
+module.exports = function gulpCleanCSS(options, callback) {
 
-  return new Transform({
-    objectMode: true,
-    transform: function modifyContents(file, enc, cb) {
-      var run = new VinylBufferStream(function(buf, done) {
-        var fileOptions = objectAssign({target: file.path}, options);
-
-        // https://github.com/jakubpawlowicz/clean-css/blob/v3.3.0/bin/cleancss#L83-L84
-        if (fileOptions.relativeTo === undefined && (fileOptions.root || file.path)) {
-          fileOptions.relativeTo = path.dirname(path.resolve(options.root || file.path));
-        }
-
-        if ((options.sourceMap === true || options.sourceMap === undefined) && file.sourceMap) {
-          fileOptions.sourceMap = JSON.stringify(file.sourceMap);
-        }
-
-        var cssFile;
-
-        if (file.path) {
-          cssFile = {};
-          cssFile[file.path] = {styles: buf.toString()};
+    if (arguments.length === 1) {
+        if (Object.prototype.toString.call(arguments[0]) == '[object Function]') {
+            callback = arguments[0];
         } else {
-          cssFile = buf.toString();
+            options = options || {};
         }
+    } else {
+        options = options || {};
+    }
 
-        new CleanCSS(fileOptions).minify(cssFile, function(errors, css) {
-          if (errors) {
-            done(errors.join(' '));
-            return;
-          }
+    return new Transform({
+        objectMode: true,
+        transform: function modifyContents(file, enc, cb) {
 
-          if (css.sourceMap) {
-            var map = JSON.parse(css.sourceMap);
-            map.file = path.relative(file.base, file.path);
-            map.sources = map.sources.map(function(src) {
-              if (/^(https?:)?\/\//.test(src)) {
-                return src;
-              }
+            var self = this;
 
-              return path.relative(file.base, src);
+            var run = new VinylBufferStream(function (buf, done) {
+                var fileOptions = objectAssign({target: file.path}, options);
+
+                if (fileOptions.relativeTo === undefined && (fileOptions.root || file.path)) {
+                    fileOptions.relativeTo = path.dirname(path.resolve(options.root || file.path));
+                }
+
+                if ((options.sourceMap === true || options.sourceMap === undefined) && file.sourceMap) {
+                    fileOptions.sourceMap = JSON.stringify(file.sourceMap);
+                }
+
+                var cssFile;
+
+                if (file.path) {
+                    cssFile = {};
+                    cssFile[file.path] = {styles: buf.toString()};
+                } else {
+                    cssFile = buf.toString();
+                }
+
+                new CleanCSS(fileOptions).minify(cssFile, function (errors, css) {
+
+                    if (errors) {
+                        done(errors.join(' '));
+                        return;
+                    }
+
+                    if (css.sourceMap) {
+                        var map = JSON.parse(css.sourceMap);
+                        map.file = path.relative(file.base, file.path);
+                        map.sources = map.sources.map(function (src) {
+                            if (/^(https?:)?\/\//.test(src)) {
+                                return src;
+                            }
+
+                            return path.relative(file.base, src);
+                        });
+
+                        applySourceMap(file, map);
+                    }
+
+                    if (typeof callback === 'function') {
+                        var details = {
+                            'stats': css.stats,
+                            'errors': css.errors,
+                            'warnings': css.warnings
+                        }
+
+                        if (css.sourceMap)
+                            details['sourceMap'] = css.sourceMap;
+
+                        callback(details);
+                    }
+
+                    done(null, new Buffer(css.styles));
+                });
             });
 
-            applySourceMap(file, map);
-          }
+            run(file, function (err, contents) {
 
-          done(null, new Buffer(css.styles));
-        });
-      });
-
-      var self = this;
-
-      run(file, function(err, contents) {
-        if (err) {
-          self.emit('error', new PluginError('gulp-clean-css', err, {fileName: file.path}));
-        } else {
-          file.contents = contents;
-          self.push(file);
+                if (err) {
+                    self.emit('error', new PluginError('gulp-clean-css', err, {fileName: file.path}));
+                } else {
+                    file.contents = contents;
+                    self.push(file);
+                }
+                cb();
+            });
         }
-        cb();
-      });
-    }
-  });
+    });
 };
