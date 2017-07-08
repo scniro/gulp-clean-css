4a4daa19fe2703b409ae60b9b8a6b59e81ac094d
Sal Niro <salvatore.niro@outlook.com>
2017-07-08 08:42:47 -0400

    clean-css bump

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var istanbul = require('gulp-istanbul');
 
 gulp.task('pre-test', function () {
   return gulp.src(['index.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+  .pipe(istanbul())
+  .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function () {
   return gulp.src('test/*.js')
-    .pipe(mocha({reporter: 'dot'}))
-    .pipe(istanbul.writeReports({
-      includeUntested: true,
-      reporters: ['lcov']
-    }))
-    .on('error', gutil.log);
+  .pipe(mocha({reporter: 'dot'}))
+  .pipe(istanbul.writeReports({
+    includeUntested: true,
+    reporters: ['lcov']
+  }))
+  .on('error', gutil.log);
 });
